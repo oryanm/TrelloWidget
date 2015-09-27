474d73b0e7244e5da8e773c3f94cbdf2fd2cc766
oryanm <oryan.mat@gmail.com>
2015-09-27 10:22:29 +0300

    fix infinite loop in ConfigActivity when not logged in

--- app/src/main/java/com/github/oryanmat/trellowidget/activity/ConfigActivity.java
+++ app/src/main/java/com/github/oryanmat/trellowidget/activity/ConfigActivity.java
@@ -5,17 +5,19 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.github.oryanmat.trellowidget.R;
 import com.github.oryanmat.trellowidget.TrelloWidget;
 import com.github.oryanmat.trellowidget.model.Board;
 import com.github.oryanmat.trellowidget.model.BoardList;
-import com.github.oryanmat.trellowidget.util.HttpErrorListener;
 import com.github.oryanmat.trellowidget.util.Json;
 import com.github.oryanmat.trellowidget.util.OnItemSelectedAdapter;
 import com.github.oryanmat.trellowidget.util.TrelloAPIUtil;
@@ -23,6 +25,7 @@ import com.github.oryanmat.trellowidget.widget.TrelloWidgetProvider;
 
 import static android.appwidget.AppWidgetManager.EXTRA_APPWIDGET_ID;
 import static android.appwidget.AppWidgetManager.INVALID_APPWIDGET_ID;
+import static com.github.oryanmat.trellowidget.TrelloWidget.T_WIDGET;
 
 public class ConfigActivity extends Activity {
     int appWidgetId = INVALID_APPWIDGET_ID;
@@ -57,15 +60,25 @@ public class ConfigActivity extends Activity {
         dialog.show();
     }
 
-    void get(String url, Response.Listener<String> listener) {
-        TrelloAPIUtil.instance.getAsync(url, listener, new HttpErrorListener(this));
+    void get(String url, BoardListener listener) {
+        TrelloAPIUtil.instance.getAsync(url, listener, listener);
     }
 
-    class BoardListener implements Response.Listener<String> {
+    class BoardListener implements Response.Listener<String>, Response.ErrorListener {
         @Override
         public void onResponse(String response) {
             Board[] boards = Json.tryParseJson(response, Board[].class, new Board[]{});
             setSpinner(R.id.boardSpinner, boards, new BoardsItemSelected());
+        }
+
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            dialog.dismiss();
+            finish();
+
+            Log.e(T_WIDGET, error.toString());
+            String text = String.format(getString(R.string.board_load_fail), error);
+            Toast.makeText(ConfigActivity.this, text, Toast.LENGTH_LONG).show();
         }
     }
 
