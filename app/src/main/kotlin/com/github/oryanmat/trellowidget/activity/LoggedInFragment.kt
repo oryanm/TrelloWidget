db118f16c6d1e288d3afafd2def2c8af76f97223
Oryan M <oryan.mat@gmail.com>
2016-11-05 20:32:10 -0400

    remove inner classes

--- app/src/main/kotlin/com/github/oryanmat/trellowidget/activity/LoggedInFragment.kt
+++ app/src/main/kotlin/com/github/oryanmat/trellowidget/activity/LoggedInFragment.kt
@@ -16,12 +16,13 @@ import com.github.oryanmat.trellowidget.util.Json
 import com.github.oryanmat.trellowidget.util.TrelloAPIUtil
 import kotlinx.android.synthetic.main.fragment_logged_in.view.*
 
-class LoggedInFragment : Fragment() {
-    val USER = "com.github.oryanmat.trellowidget.activity.user"
-    val VISIBILITY = "com.github.oryanmat.trellowidget.activity.visibility"
-    val MAX_LOGIN_FAIL = 3
+class LoggedInFragment : Fragment(), Response.Listener<String>, Response.ErrorListener {
+    private val USER = "com.github.oryanmat.trellowidget.activity.user"
+    private val VISIBILITY = "com.github.oryanmat.trellowidget.activity.visibility"
+    private val MAX_LOGIN_FAIL = 3
 
-    internal var user = User()
+    private var user = User()
+    private var count = 1
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_logged_in, container, false)
@@ -35,36 +36,28 @@ class LoggedInFragment : Fragment() {
             val userJson = savedInstanceState?.getString(USER) ?: ""
             setUser(Json.tryParseJson(userJson, User::class.java, User()))
         } else {
-            TrelloAPIUtil.instance.getUserAsync(UserListener(), LoginErrorListener())
+            TrelloAPIUtil.instance.getUserAsync(this, this)
         }
     }
 
-    internal inner class LoginErrorListener : Response.ErrorListener {
-        var count = 1
+    override fun onErrorResponse(error: VolleyError) {
+        Log.e(T_WIDGET, error.toString())
 
-        override fun onErrorResponse(error: VolleyError) {
-            Log.e(T_WIDGET, error.toString())
-
-            if (count >= MAX_LOGIN_FAIL) {
-                // if user get request failed N times then logout so user can try to login again
-                if (isAdded) {
-                    (activity as MainActivity).logout()
-                    val text = String.format(activity.getString(R.string.login_fail), error)
-                    Toast.makeText(activity, text, Toast.LENGTH_LONG).show()
-                }
-            } else {
-                // try again. could be temp problem
-                count += 1
-                TrelloAPIUtil.instance.getUserAsync(UserListener(), this)
+        if (count >= MAX_LOGIN_FAIL) {
+            // if user get request failed N times then logout so user can try to login again
+            if (isAdded) {
+                (activity as MainActivity).logout()
+                val text = String.format(activity.getString(R.string.login_fail), error)
+                Toast.makeText(activity, text, Toast.LENGTH_LONG).show()
             }
+        } else {
+            // try again. could be temp problem
+            count++
+            TrelloAPIUtil.instance.getUserAsync(this, this)
         }
     }
 
-    internal inner class UserListener : Response.Listener<String> {
-        override fun onResponse(response: String) {
-            setUser(Json.tryParseJson(response, User::class.java, User()))
-        }
-    }
+    override fun onResponse(response: String) = setUser(Json.tryParseJson(response, User::class.java, User()))
 
     private fun setUser(user: User) {
         this.user = user
