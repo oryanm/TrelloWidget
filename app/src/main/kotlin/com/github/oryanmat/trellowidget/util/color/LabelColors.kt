3850c485e8f75594f53b9f5fb29a6125c3972f2f
Oryan M <oryan.mat@pm.me>
2024-12-03 07:26:10 -0500

    Add missing label colors

--- app/src/main/kotlin/com/github/oryanmat/trellowidget/util/color/LabelColors.kt
+++ app/src/main/kotlin/com/github/oryanmat/trellowidget/util/color/LabelColors.kt
@@ -2,26 +2,38 @@ package com.github.oryanmat.trellowidget.util.color
 
 import android.graphics.Color
 
-const val PINK = "#FF80CE"
-const val ORANGE = "#FFAB4A"
-const val LIME = "#51E898"
-const val SKY = "#00C2E0"
-const val BLUE = "#0079BF"
-const val GREEN = "#61BD4F"
-const val YELLOW = "#F2D600"
-const val RED = "#EB5A46"
-const val PURPLE = "#C377E0"
-const val LIGHT_GREY = "#C4C9CC"
-
+// TODO colors are based on Trello's light theme
+//  Consider adding an option to switch between a light and dark set of label colors
 val colors = mapOf(
-        "black" to Color.BLACK,
-        "purple" to Color.parseColor(PURPLE),
-        "pink" to Color.parseColor(PINK),
-        "red" to Color.parseColor(RED),
-        "orange" to Color.parseColor(ORANGE),
-        "yellow" to Color.parseColor(YELLOW),
-        "green" to Color.parseColor(GREEN),
-        "lime" to Color.parseColor(LIME),
-        "sky" to Color.parseColor(SKY),
-        "blue" to Color.parseColor(BLUE),
-        null to Color.parseColor(LIGHT_GREY))
+    "black" to Color.parseColor("#8590A2"),
+    "black_dark" to Color.parseColor("#626F86"),
+    "black_light" to Color.parseColor("#DCDFE4"),
+    "blue" to Color.parseColor("#579DFF"),
+    "blue_dark" to Color.parseColor("#0C66E4"),
+    "blue_light" to Color.parseColor("#CCE0FF"),
+    "green" to Color.parseColor("#4BCE97"),
+    "green_dark" to Color.parseColor("#1F845A"),
+    "green_light" to Color.parseColor("#BAF3DB"),
+    "lime" to Color.parseColor("#94C748"),
+    "lime_dark" to Color.parseColor("#5B7F24"),
+    "lime_light" to Color.parseColor("#D3F1A7"),
+    "orange" to Color.parseColor("#FEA362"),
+    "orange_dark" to Color.parseColor("#C25100"),
+    "orange_light" to Color.parseColor("#FEDEC8"),
+    "pink" to Color.parseColor("#E774BB"),
+    "pink_dark" to Color.parseColor("#AE4787"),
+    "pink_light" to Color.parseColor("#FDD0EC"),
+    "purple" to Color.parseColor("#9F8FEF"),
+    "purple_dark" to Color.parseColor("#6E5DC6"),
+    "purple_light" to Color.parseColor("#DFD8FD"),
+    "red" to Color.parseColor("#F87168"),
+    "red_dark" to Color.parseColor("#C9372C"),
+    "red_light" to Color.parseColor("#FFD5D2"),
+    "sky" to Color.parseColor("#6CC3E0"),
+    "sky_dark" to Color.parseColor("#227D9B"),
+    "sky_light" to Color.parseColor("#C6EDFB"),
+    "yellow" to Color.parseColor("#F5CD47"),
+    "yellow_dark" to Color.parseColor("#946F00"),
+    "yellow_light" to Color.parseColor("#F8E6A0"),
+    null to Color.parseColor("#DCDFE4") // black_light
+)
